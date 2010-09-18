b15418c84769072e996adb9d1354a43fe65735e3
Fil <fil@rezo.net>
2010-09-18 16:25:40 +0000

    je pallie à l'absence d'accents sur le clavier de cerdic

--- lang/dump_fr.php
+++ lang/dump_fr.php
@@ -1,16 +1,16 @@
-<?php
+﻿<?php
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'label_nom_fichier_sauvegarde' => "Nom du fichier de sauvegarde",
-	'label_nom_fichier_restaurer' => 'Ou indiquez le nom du fichier a restaurer',
-	'label_selectionnez_fichier' => 'Selectionnez un fichier dans la liste',
+	'label_nom_fichier_sauvegarde' => "Fichier de sauvegarde",
+	'label_nom_fichier_restaurer' => 'Ou indiquez le nom du fichier &#224; restaurer',
+	'label_selectionnez_fichier' => 'S&#233;lectionnez un fichier dans la liste',
 
 	'erreur_taille_sauvegarde' => 'La sauvegarde semble avoir &eacute;chou&eacute;. Le fichier @fichier@ est vide ou absent.',
 	'erreur_nom_fichier' => 'Ce nom de fichier n\'est pas autoris&eacute;',
-	'erreur_sauvegarde_deja_en_cours' => 'Vous avez d&eacute;j&agrave; une sauvegarde en cours !',
+	'erreur_sauvegarde_deja_en_cours' => 'Vous avez d&eacute;j&agrave; une sauvegarde en cours',
 	'erreur_sqlite_indisponible' => 'Impossible de faire une sauvegarde SQLite sur votre h&eacute;bergement',
 	'erreur_creation_base_sqlite' => 'Impossible de cr&eacute;er une base SQLite pour la sauvegarde',
 	'erreur_connect_dump' => 'Un serveur nomm&eacute; &#171;&#160;@dump@&#160;&#187; existe d&eacute;j&agrave;. Renommez-le.',
@@ -20,15 +20,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'tout_sauvegarder' => 'Sauvegarder toutes les tables',
 	'tout_restaurer' => 'Restaurer toutes les tables',
-	'une_donnee' => '1 enregistrement',
-	'nb_donnees' => '@nb@ enregistrements',
+	'une_donnee' => '1&nbsp;enregistrement',
+	'nb_donnees' => '@nb@&nbsp;enregistrements',
 	'aucune_donnee' => 'vide',
-	'info_taille' => 'Taille',
+	'info_taille' => 'Taille', # ???
 	'sauvegardes_existantes' => 'Sauvegardes existantes',
-	'info_selection_sauvegarde' => 'Vous avez choisi de restaurer la sauvegarde @fichier@. Cette opération est irreversible.',
-	'confirmer_ecraser_base' => 'Oui je veux ecraser ma base avec cette sauvegarde',
-	'confirmer_ecraser_tables_selection' => 'Oui je veux ecraser les tables selectionn&eacute;es avec cette sauvegarde',
-	'selectionnez_table_a_restaurer' => 'Selectionnez les tables qui seront restaur&eacute;es',
+	'info_selection_sauvegarde' => 'Vous avez choisi de restaurer la sauvegarde @fichier@. Cette op&#233;ration est irr&#233;versible.',
+	'confirmer_ecraser_base' => 'Oui, je veux &#233;craser ma base avec cette sauvegarde',
+	'confirmer_ecraser_tables_selection' => 'Oui, je veux &#233;craser les tables s&#233;lectionn&eacute;es avec cette sauvegarde',
+	'selectionnez_table_a_restaurer' => 'S&#233;lectionnez les tables &#224; restaurer',
 
 );
 
