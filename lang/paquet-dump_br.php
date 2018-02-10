da48a3dda01078816e2cbbe0b3c593ff1b1905ec
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-10 04:36:35 +0000

    [Salvatore] [source:_core_/plugins/dump/lang/ paquet-dump] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-dump_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-dump?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'dump_description' => 'Gwared hag assevel deus an diaz titouroù e SQLite ',
+	'dump_slogan' => 'Gwared hag assevel deus an diaz titouroù SPIP'
+);
