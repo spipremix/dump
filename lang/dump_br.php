5a68b98d5ec36071661fd822bb96f942d4460aca
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-26 04:36:31 +0000

    [Salvatore] [source:_core_/plugins/dump/lang/ dump] Export depuis http://trad.spip.net de la langue br

--- lang/dump_br.php
+++ lang/dump_br.php
@@ -9,15 +9,52 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'aucune_donnee' => 'goullo',
+	'avis_probleme_ecriture_fichier' => 'Fazi en ur skrivañ ar restr @fichier@',
+
 	// B
 	'bouton_restaurer_base' => 'Assevel an diaz',
 
+	// C
+	'confirmer_ecraser_base' => 'Ya, c’hoant em eus flastrañ ma diaz gant ar saveteerezh-mañ',
+	'confirmer_ecraser_tables_selection' => 'Ya, c’hoant em eus flastrañ gant ar saveteerezh-mañ an taolioù dibabet ',
+	'confirmer_supprimer_sauvegarde' => 'C’hoant hoc’h eus dilemel ar saveteerezh-mañ ?',
+
+	// D
+	'details_sauvegarde' => 'Munudoù an saveteerezh :',
+
+	// E
+	'erreur_aucune_donnee_restauree' => 'Roadenn assevet ebet',
+	'erreur_creation_base_sqlite' => 'Ne c’haller ket krouiñ un diaz SQLite evit an saveteerezh',
+	'erreur_restaurer_verifiez' => 'Reizhit ar fazi evit adsevel.',
+	'erreur_sauvegarde_deja_en_cours' => 'Ur saveteerezh war ober hoc’h eus a-vremañ',
+	'erreur_table_absente' => 'Taol @table@ ebet',
+	'erreur_table_donnees_manquantes' => 'Mankout a ra roadennoù, taol @table@',
+
 	// I
+	'info_aucune_sauvegarde_trouvee' => 'Saveteerezh ebet kavet',
+	'info_restauration_finie' => 'Echu eo !. Ar saveteerezg @archive@ a zo bet adsevet en ho lec’h. Gallout a rit',
 	'info_restauration_sauvegarde' => 'Assevel ar restr savete @archive@',
-	'info_sauvegarde' => 'Saveteiñ',
+	'info_sauvegarde' => 'Saveteerezh',
 	'info_sauvegarde_reussi_02' => 'Enrollet eo bet an diaz titouroù e @archive@. Gallout a rit',
 	'info_sauvegarde_reussi_03' => 'distreiñ da verañ',
-	'info_sauvegarde_reussi_04' => 'ho lec’hienn.',
+	'info_sauvegarde_reussi_04' => 'deus ho lec’hienn.',
+
+	// L
+	'label_nom_fichier_sauvegarde' => 'Anv ar restr evit ar saveteerezh',
+	'label_selectionnez_fichier' => 'Dibabit ur restr er roll',
+
+	// N
+	'nb_donnees' => '@nb@ enrolladennoù',
+
+	// R
+	'restauration_en_cours' => 'Adsevelezh war ober',
+
+	// S
+	'sauvegarde_en_cours' => 'Saveteerezh war ober',
+	'sauvegardes_existantes' => 'Saveteerezhoù bev',
+	'selectionnez_table_a_restaurer' => 'Dibabit an taolioù o adsevel',
 
 	// T
 	'texte_admin_tech_01' => 'Gant an dibarzh-mañ e c’hellit saveteiñ endalc’had an diaz titouroù en ur restr a vo stoket e @dossier@. N’ankouait ket kennebeut da adtapout hollad ar c’havlec’h @img@ hag a zo ennañ ar skeudennoù hag an teulioù implijet er pennadoù hag er rubrikennoù.',
@@ -27,5 +64,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   Evit hen ober e rank ar restr savete bezañ bet lakaet er c’havlec’h @dossier@.
   Diwallit gant an arc’hwel-mañ, n’haller ket mont war-gil ma vez diverket pe gwallgaset tra pe dra.</b>', # MODIF
 	'texte_sauvegarde' => 'Saveteiñ endalc’had an diaz',
-	'texte_sauvegarde_base' => 'Saveteiñ an diaz'
+	'texte_sauvegarde_base' => 'Saveteiñ an diaz',
+	'tout_restaurer' => 'Adsevel an holl taolioù',
+	'tout_sauvegarder' => 'Saveteiñ an holl taolioù',
+
+	// U
+	'une_donnee' => '1 enrolladenn'
 );
